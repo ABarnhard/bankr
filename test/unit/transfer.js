32eb7c1ce863e1609cda26705e1c4fdcccdd8c41
Adam Barnhard <admbarn@gmail.com>
2014-08-08 17:27:16 -0500

    updated tests

---
+++ test/unit/transfer.js
@@ -0,0 +1,15 @@
+/* jshint expr:true */
+/* global describe, it */
+
+'use strict';
+
+var expect = require('chai').expect;
+var Transfer = require('../../app/models/transfer');
+
+describe('Transfer', function(){
+  describe('Controller', function(){
+    it('Should create a new transfer object with correct properties', function(){
+    
+    });
+  });
+});
